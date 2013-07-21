ff2dcbdcc34711a9860b9035cf5faca5b3c4d751
JP DeVries <johnpaul.devries@gmail.com>
2013-07-20 18:18:12 -0700

    Changing Icon
    to wand

--- style.js
+++ style.js
@@ -3,22 +3,27 @@ if (!RedactorPlugins) var RedactorPlugins = {};
 RedactorPlugins.style = {
 	init: function ()
 	{
-		var styleJSON = [{'name':'Bold Red','className':'redactor_format_boldred','style':'font-weight:bold;color:red'}];
+		console.dir(this.opts.formattingTags);
+		var styleJSON = [
+			{btnName:'Bold Red',className:'redactor_format_boldred',style:'font-weight:bold;color:red'},
+			{btnName:'aside',className:'redactor_format_aside',wrap: 'aside'}
+		];
 		var that = this;
 		var dropdown = {};
 
 		$.each(styleJSON, function(i, s)
 		{
-			dropdown['s' + i] = { title: s.name, className:s.className, callback: function() { that.setFontfamily(s.style); }};
+			dropdown['s' + i] = { title: s.btnName, className:s.className, callback: function() { that.setFontfamily(s); }};
 		});
 
 		dropdown['remove'] = { title: 'Remove style', callback: function() { that.resetFontfamily(); }};
 
 		this.buttonAddAfter('formatting','style', 'Change font family', false, dropdown);
 	},
-	setFontfamily: function (value)
+	setFontfamily: function (s)
 	{
-		this.inlineSetAttr('style', value);
+		if (s.wrap) this.selectionWrap(s.wrap);
+		else this.inlineSetAttr('style', s.style);
 	},
 	resetFontfamily: function()
 	{
